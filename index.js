29ea9e53b61fb59e2b56f3eefb67f445e9b44ce1
stelios <stelios_tsita@hotmail.com>
2025-03-15 03:30:39 +0200

    major changes to the ui

--- index.js
+++ index.js
@@ -1,38 +1,11 @@
-function newTab(url) {
-  window.open(url, "_blank");
-}
-
-function switchLanguage(lang) {
-  const englishContent = document.getElementById("englishContent");
-  const greekContent = document.getElementById("greekContent");
-  const languageButtonEN = document.getElementById("languageButtonEN");
-  const languageButtonGR = document.getElementById("languageButtonGR");
-
-  // Εμφάνιση περιεχομένου ανάλογα με τη γλώσσα
-  englishContent.style.display = lang === "en" ? "block" : "none";
-  greekContent.style.display = lang === "gr" ? "block" : "none";
-
-  // Ενημέρωση τίτλου κουμπιών
-  languageButtonEN.title =
-    lang === "en" ? "This page is in English" : "Κλικ για προβολή στα Αγγλικά";
-  languageButtonGR.title =
-    lang === "gr"
-      ? "Η σελίδα είναι στα ελληνικά"
-      : "Click to switch to English";
-
-  if (lang === "en") {
-    greekContent.classList.remove("active");
-    setTimeout(() => {
-      greekContent.style.display = "none";
-      englishContent.style.display = "block";
-      englishContent.classList.add("active");
-    }, 10);
-  } else if (lang === "gr") {
-    englishContent.classList.remove("active");
-    setTimeout(() => {
-      englishContent.style.display = "none";
-      greekContent.style.display = "block";
-      greekContent.classList.add("active");
-    }, 10);
-  }
-}
+document.addEventListener("DOMContentLoaded", function() {
+            const socialBtns = document.querySelectorAll(".social-btn");
+            socialBtns.forEach(btn => {
+                btn.addEventListener("mouseover", () => {
+                    btn.style.transform = "scale(1.1)";
+                });
+                btn.addEventListener("mouseout", () => {
+                    btn.style.transform = "scale(1)";
+                });
+            });
+        });
